b51362038d4e80b20ebd03c5240537d42cc7e323
morrisRio <maurice.rio@gmx.de>
2023-05-16 16:07:31 +0200

    endstand

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,81 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+        import { onMount } from 'svelte';
+        import { browser } from '$app/environment';
+
+        let todoText = '';
+        let todos = [];
+
+        onMount(() => {
+                if (browser) {
+                        const storedTodos = JSON.parse(
+                                localStorage.getItem('todos')
+                        );
+                        if (storedTodos) {
+                                todos = storedTodos;
+                        }
+                }
+        });
+
+        function saveTodos() {
+                // Save todos to local storage
+                if (browser) {
+                        localStorage.setItem('todos', JSON.stringify(todos));
+                }
+        }
+
+        function addTodo() {
+                todos.push({ text: todoText, done: false });
+                todos = todos;
+                todoText = '';
+                // Save todos to local storage
+                saveTodos();
+        }
+
+        function remove(index) {
+                //delete entry
+                console.log(index);
+                todos.splice(index, 1);
+                todos = todos;
+                saveTodos();
+        }
+</script>
+
+<h1>TODO APP</h1>
+
+<!-- textfeld -->
+<input type="text" class="todo-input" bind:value={todoText} />
+<!-- button add -->
+<button on:click={addTodo}>ADD</button>
+
+{#each todos as todo, i}
+        <!-- todo -->
+        <div class="todo-entry" class:done={todo.done}>
+                <!-- text -->
+                <div>{todo.text}</div>
+                <!-- checkboxen -->
+                <input type="checkbox" bind:checked={todo.done} />
+                <button
+                        class="delete"
+                        on:click={() => {
+                                remove(i);
+                        }}>X</button
+                >
+        </div>
+{/each}
+
+<style>
+        .delete {
+                background-color: white;
+                border: none;
+        }
+        .delete:hover {
+                background-color: grey;
+                font-weight: 700;
+        }
+        .done {
+                color: grey;
+        }
+        .todo-entry {
+                display: flex;
+        }
+</style>
